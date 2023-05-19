ab697bcc300c8367ffd783fec71c776c064958ee
gimdaeyeon <rlaeodus1306@naver.com>
2023-05-19 10:00:52 +0900

    study : 최종예제app프로젝트 생성 및 초기 설정

---
+++ workspace/app/src/main/java/com/example/app/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.app.controller;
+
+import com.example.app.service.BoardService;
+import com.example.app.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+}
