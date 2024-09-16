3abd60c01a95f88e7b15f04ef4328cc12275cc04
gimdaeyeon <rlaeodus1306@naver.com>
2024-09-16 16:18:47 +0900

    Fix: Period createdDate, modifiedDate auditing 설정

--- workspace/JWT2/src/main/java/com/security/jwt2/domain/base/Period.java
+++ workspace/JWT2/src/main/java/com/security/jwt2/domain/base/Period.java
@@ -1,13 +1,18 @@
 package com.security.jwt2.domain.base;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter @ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Period {
     @CreatedDate
     private LocalDateTime createdDate;
