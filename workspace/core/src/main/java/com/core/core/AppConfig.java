0cd157e0ad2d42fa7ded8b19f846b783f4681587
gimdaeyeon <rlaeodus1306@naver.com>
2024-09-26 23:37:36 +0900

    Feat[spring core]: AppConfig 로 할인 정책 교체

--- workspace/core/src/main/java/com/core/core/AppConfig.java
+++ workspace/core/src/main/java/com/core/core/AppConfig.java
@@ -1,6 +1,9 @@
 package com.core.core;
 
+import com.core.core.discount.DiscountPolicy;
 import com.core.core.discount.FixDiscountPolicy;
+import com.core.core.discount.RateDiscountPolicy;
+import com.core.core.member.MemberRepository;
 import com.core.core.member.MemberService;
 import com.core.core.member.MemberServiceImpl;
 import com.core.core.member.MemoryMemberRepository;
@@ -9,10 +12,19 @@ import com.core.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new FixDiscountPolicy(),new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(),memberRepository());
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
