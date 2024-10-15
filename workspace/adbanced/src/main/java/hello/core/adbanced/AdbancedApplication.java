fa8e558233287c583422556c14434394383a7c3a
gimdaeyeon <rlaeodus1306@naver.com>
2024-10-15 20:52:50 +0900

    Init: Spring core 고급편 학습을 위한 예제 프로젝트 생성

---
+++ workspace/adbanced/src/main/java/hello/core/adbanced/AdbancedApplication.java
@@ -0,0 +1,13 @@
+package hello.core.adbanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdbancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdbancedApplication.class, args);
+    }
+
+}
---
+++ workspace/adbanced/src/test/java/hello/core/adbanced/AdbancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.core.adbanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdbancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
