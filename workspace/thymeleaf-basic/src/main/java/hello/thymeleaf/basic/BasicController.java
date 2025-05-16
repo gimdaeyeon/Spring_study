f50d3b682da486d23c8dea3e158d45dc390e98e7
gimdaeyeon <rlaeodus1306@naver.com>
2025-05-16 23:20:04 +0900

    Study: 타임리프 기본 객체, 유틸리티 객체, 날짜 관련 학습

--- workspace/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ workspace/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,12 +1,15 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -15,19 +18,19 @@ import java.util.Map;
 public class BasicController {
 
     @GetMapping("text-basic")
-    public String textBasic(Model model){
-        model.addAttribute("data","Hello Spring!");
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
         return "basic/text-basic";
     }
 
     @GetMapping("text-unescaped")
-    public String textUnescaped(Model model){
-        model.addAttribute("data","Hello <b>Spring!</b>");
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
     }
 
     @GetMapping("variable")
-    public String variable(Model model){
+    public String variable(Model model) {
         User userA = new User("userA", 10);
         User userB = new User("userB", 20);
 
@@ -35,15 +38,35 @@ public class BasicController {
 
         Map<String, User> map = Map.of("userA", userA, "userB", userB);
 
-        model.addAttribute("user",userA);
-        model.addAttribute("users",list);
-        model.addAttribute("userMap",map);
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
         return "basic/variable";
     }
 
+    @GetMapping("basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData","Hello Session");
+        return "basic/basic-objects";
+    }
 
-    @Data @AllArgsConstructor
-    static class User{
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class User {
         private String username;
         private int age;
     }
