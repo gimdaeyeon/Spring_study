4646181675cf3b99215651842d632f976e3f0226
gimdaeyeon <rlaeodus1306@naver.com>
2025-05-31 00:18:04 +0900

    Study: Spring Bean Validation 에러 코드, 오브젝트 오류 학습

--- workspace/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ workspace/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript",script = "_this.price * _this.quantity >= 100000", message = "총합이 10000원 넘게 입력해 주세요")
 public class Item {
 
     private Long id;
